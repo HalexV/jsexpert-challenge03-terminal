b7803ba5e1431caefca067b90ec25725956fb952
Halex Viotto Gomes <halexx002@gmail.com>
2022-01-06 16:56:28 -0400

    test: ensure axios is called with the url passed to makeRequest

--- test/unit/IncomeRepository.test.js
+++ test/unit/IncomeRepository.test.js
@@ -1,6 +1,13 @@
 import { describe, it, before } from 'mocha';
 import { expect } from 'chai';
+import sinon from 'sinon'
 import { incomeRepositoryMock, mocks } from '../mocks/incomeRepository.mock.js';
+import axios from 'axios'
+import IncomeRepository from '../../src/repository/IncomeRepository.js';
+
+const axiosStub = sinon.stub(axios, 'get').callsFake( async () => {
+  return new Promise(resolve => resolve({data: 'ok'}))
+} )
 
 describe('IncomeRepository Suite Tests', () => {
   let repository = {};
@@ -8,6 +15,14 @@ describe('IncomeRepository Suite Tests', () => {
   before(() => {
     repository = incomeRepositoryMock;
   });
+
+  it('should call axios with the url passed to makeRequest', async () => {
+    const sut = new IncomeRepository()
+    await sut.makeRequest('valid_url')
+    
+    expect(axiosStub.calledOnce).to.be.ok
+    expect(axiosStub.calledWith('valid_url')).to.be.ok
+  })
 
   it('should return the correct list of conversions when getConversions is called', async () => {
     const expected = mocks.convertResponse.results;
