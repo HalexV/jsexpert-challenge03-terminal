f65237f596bab29f75ce3a7a818577f1e8d52e34
Halex Viotto Gomes <halexx002@gmail.com>
2022-01-07 10:12:09 -0400

    feat: it should return an object containing a data and a status field

--- src/repository/IncomeRepository.js
+++ src/repository/IncomeRepository.js
@@ -3,11 +3,12 @@ import axios from 'axios'
 const API_BASE_URL = 'http://localhost:3000';
 
 axios.defaults.baseURL = API_BASE_URL
+axios.defaults.validateStatus = null
 
 class IncomeRepository {
   async makeRequest(url) {
-    const response = await axios.get(url)
-    return response.data;
+    const {data, status} = await axios.get(url)
+    return {data, status};
   }
 
   async getConversions() {
