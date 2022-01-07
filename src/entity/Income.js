8ab842af9e1351a0a0f606a1f73cc7e91c674fae
Halex Viotto Gomes <halexx002@gmail.com>
2022-01-07 15:54:44 -0400

    feat: static formatCurrency should throw if currency is not a string

--- src/entity/Income.js
+++ src/entity/Income.js
@@ -3,6 +3,8 @@ import languageConfig from '../config/language.js';
 const defaultLanguage = languageConfig.default;
 
 class Income {
+  static #id = 0;
+  
   constructor({
     position,
     expectation,
@@ -10,16 +12,20 @@ class Income {
     conversion02,
     conversion03,
   }) {
-    this.position = position;
-    this.expectation = expectation || { currency: 'BRL', language: 'pt-BR' };
-    this.conversion01 = conversion01 || { currency: 'USD', language: 'en-US' };
-    this.conversion02 = conversion02 || { currency: 'EUR', language: 'en-GB' };
-    this.conversion03 = conversion03 || { currency: 'RUB', language: 'ru-RU' };
+    this.position = position || 'No description';
+    this.expectation = expectation || { currency: 'BRL', language: 'pt-BR', value: 100.5 };
+    this.conversion01 = conversion01 || { currency: 'USD', language: 'en-US', value: 100.5 };
+    this.conversion02 = conversion02 || { currency: 'EUR', language: 'en-GB', value: 100.5 };
+    this.conversion03 = conversion03 || { currency: 'RUB', language: 'ru-RU', value: 100.5 };
+  }
+
+  #getId() {
+    return ++Income.#id
   }
 
   format() {
     return {
-      id: this.id,
+      id: this.#getId(),
       position: this.position,
       expectation: Income.formatCurrency(this.expectation),
       conversion01: Income.formatCurrency(this.conversion01),
@@ -30,6 +36,8 @@ class Income {
 
   static formatCurrency({ currency, value, language }) {
     const _language = language || defaultLanguage;
+
+    if (typeof currency !== 'string') throw new Error('Currency must be a string')
 
     // TODO: Implement method
     return null;
