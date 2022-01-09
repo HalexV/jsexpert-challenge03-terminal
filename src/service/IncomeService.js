b7885b8ba72456800c6cbbfee0085cfa6538d346
Halex Viotto Gomes <halexx002@gmail.com>
2022-01-09 19:18:22 -0400

    feat: should return an error when generating an income instance from empty string

--- src/service/IncomeService.js
+++ src/service/IncomeService.js
@@ -8,7 +8,8 @@ class IncomeService {
   async generateIncomeFromString(incomeString, delimiter = ';') {
     if (typeof incomeString !== 'string') throw new Error('IncomeString must be a string')
     if (typeof delimiter !== 'string') throw new Error('Delimiter must be a string')
-    
+    if (!incomeString) throw new Error('Position is a required field. Please make sure you are providing a position.')
+
     const [position, expectation] = incomeString.split(delimiter);
 
     // @TODO: Implement method
