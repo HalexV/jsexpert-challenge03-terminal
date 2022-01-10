f8061855e599ece573ebf405d8822549734e9be5
Halex Viotto Gomes <halexx002@gmail.com>
2022-01-10 13:36:48 -0400

    test: should call addLineListener once with process.stdin when initialize is called

--- test/unit/CustomTerminal.test.js
+++ test/unit/CustomTerminal.test.js
@@ -15,12 +15,34 @@ describe('Terminal Suite Tests', () => {
     it('should call Draftlog once with console when initialize is called', () => {
       const sut = new CustomTerminal()
 
-      const intoStub = sinon.stub(Draftlog, 'into').callsFake(() => 'ok')
+      const intoStub = sinon.stub(Draftlog, 'into').callsFake(() => ({
+        addLineListener: function () {
+          return
+        }
+      }))
 
       sut.initialize()
 
       expect(intoStub.calledOnce).to.be.ok
       expect(intoStub.calledWith(console)).to.be.ok
+    })
+
+    it('should call addLineListener once with process.stdin when initialize is called', () => {
+      const sut = new CustomTerminal()
+      let calls = 0
+      let argument = null 
+
+      sinon.stub(Draftlog, 'into').callsFake(() => ({
+        addLineListener: function () {
+          calls++
+          argument = arguments['0']
+        }
+      }))
+
+      sut.initialize()
+
+      expect(calls).to.be.equal(1)
+      expect(argument).to.be.deep.equal(process.stdin)
     })
 
   })
