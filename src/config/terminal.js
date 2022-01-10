683c9bc836d03c48d486e1bb294a2b865c909716
Halex Viotto Gomes <halexx002@gmail.com>
2022-01-10 08:36:19 -0400

    feat: create chalk table options

--- src/config/terminal.js
+++ src/config/terminal.js
@@ -1,9 +1,15 @@
 import chalk from 'chalk';
 
-// @TODO: Any guesses about what's missing?
 export default {
-  table: {
+  tableOptions: {
     leftPad: 2,
-    columns: [],
+    columns: [
+      { field: 'id', name: chalk.cyan('ID') },
+      { field: 'position', name: chalk.green('Position') },
+      { field: 'expectation', name: chalk.yellow('Expectation (BRL)') },
+      { field: 'conversion01', name: chalk.blue('USD') },
+      { field: 'conversion02', name: chalk.magenta('EUR') },
+      { field: 'conversion03', name: chalk.red('GBP') },
+    ],
   },
 };
